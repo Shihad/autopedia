bbdd33f1454c9dcac35e20109bf1416a41d9c3d7
ikit5 <ikit13@ya.ru>
2023-02-21 18:18:13 +0500

    Added update_label_api

--- forms.py
+++ forms.py
@@ -6,3 +6,12 @@ class LoginForm(FlaskForm):
     login = StringField("Введите логин", validators=[DataRequired()])
     password = PasswordField("Введите пароль", validators=[DataRequired()])
     submit = SubmitField()
+
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Логин",validators=[DataRequired()])
+    mail = StringField("Почта")
+    password1 = PasswordField("Пароль",validators=[DataRequired()])
+    password2 = PasswordField("Повтор пароля", validators=[DataRequired()])
+    submit = SubmitField()
